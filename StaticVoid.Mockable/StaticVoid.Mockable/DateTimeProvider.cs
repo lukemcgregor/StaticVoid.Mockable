81ffbd86b93fb7ad4d070324459918a4590064ce
Luke McGregor <luke@mcgregor.org.nz>
2013-04-26 21:21:36 +1200

    Datetime

---
+++ StaticVoid.Mockable/StaticVoid.Mockable/DateTimeProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticVoid.Mockable
+{
+    public class DateTimeProvider : IProvideDateTime
+    {
+        public DateTime Now
+        {
+            get { return DateTime.Now; }
+        }
+
+        public DateTime Today
+        {
+            get { return DateTime.Today; }
+        }
+    }
+
+    public class MockDateTimeProvider : IProvideDateTime
+    {
+        private readonly DateTime _now;
+        public MockDateTimeProvider(DateTime now)
+        {
+            _now = now;
+        }
+
+        public DateTime Now
+        {
+            get { return _now; }
+        }
+
+        public DateTime Today
+        {
+            get { return _now.Date; }
+        }
+    }
+}
---
+++ StaticVoid.Mockable/StaticVoid.Mockable/IProvideDateTime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticVoid.Mockable
+{
+    /// <summary>
+    /// DateTime is important part of applications and its not too easy to unit test
+    /// that things are happening as expected (you need deltas and such gumph) so this
+    /// interface lets you abstract out a few common DateTime bits so that you can 
+    /// replace them with a mock for unit testing 
+    /// </summary>
+    public interface IProvideDateTime
+    {
+        /// <summary>
+        /// Corresponds to DateTime.Now
+        /// </summary>
+        DateTime Now { get; }
+        /// <summary>
+        /// Corresponds to DateTime.Today
+        /// </summary>
+        DateTime Today { get; }
+    }
+}
